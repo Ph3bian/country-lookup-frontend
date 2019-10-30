c44d13f07f777a25fbea8765e805a64ba90a7ff3
Phebian | Kudi <kudi@kudi-inc.local>
2019-10-31 00:06:14 +0100

    updated server.js file

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/swapi'));
+app.use(express.static(__dirname + '/build'));
 
 app.get('/*', (req, res) => {
-	res.sendFile(__dirname + '/dist/swapi/index.html');
+	res.sendFile(__dirname + '/build/index.html');
 });
 
 app.listen(process.env.PORT || 7009);
